2151f7e1d87bd5b0945b1d3776e912a1a8930ef6
Mae <asmaecharroudd@gmail.com>
2025-09-05 22:08:31 +0200

    Started about changes

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -1,10 +1,23 @@
+import './About.css'
+import Portfolio_img from '../assets/Images/MaePortfolio.jpg'
+
+
 function About() {
     return (
-        <section id="about" className="about">
-            <h2>Sobre mí</h2>
-            <p>
-            </p>
-        </section>
+        <div className="About--grid">
+            <div className="About__image">
+                <img src={Portfolio_img} alt="Asmae's Image" height={550} />
+            </div>
+            <div className="About__description">
+                <div className='About__introduction'>
+                    <h2>Introduction</h2>
+                </div>
+                <div className='About__GetinT'>
+                    <h2>Get in touch</h2>
+                    <div className='About__GetinT--grid'></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
