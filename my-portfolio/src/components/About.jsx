3d8845fa22e7b286d064f60ac1f232e34077c08b
Mae <asmaecharroudd@gmail.com>
2025-09-06 15:09:44 +0200

    Added info to get in touc section

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -1,6 +1,12 @@
 import './About.css'
 import Portfolio_img from '../assets/Images/MaePortfolio.jpg'
 import Introducction_illustration from '../assets/Images/Introduction_illustration.svg'
+import Mail from '../assets/Images/Mail.png'
+import Linkedin from '../assets/Images/Linkedin.png'
+import Github from '../assets/Images/Github.png'
+
+
+
 
 function About() {
     return (
@@ -15,7 +21,7 @@ function About() {
                         <h3>A junior web developer/designer</h3>
                         <div className='Introduction__container--grid'>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eget faucibus volutpat, odio purus egestas nunc, eu pellentesque velit sem nec eros. Etiam egestas arcu eget orci scelerisque commodo. Fusce pharetra enim vitae lectus bibendum varius. Vivamus placerat aliquam lorem quis pharetra.</p>
-                            <img src={Introducction_illustration} alt="Asmae's Image" height={150} />
+                            <img src={Introducction_illustration} alt="Web development illustration" height={150} />
                         </div>
                     </div>
                 </div>
@@ -23,13 +29,19 @@ function About() {
                     <h2>Get in touch</h2>
                     <div className='About__GetinT--grid GetinT'>
                         <div className='GetinT__Mail'>
-
+                            <img src={Mail} alt="Mail icon" height={60} />
+                            <div class="linea-con-bolas"></div>
+                            <p>send me an email</p>
                         </div>
-                        <div className='GetinT__Linkedin'>
-
+                        <div href="https://www.linkedin.com/in/asmae-c-3a10b2255/" className='GetinT__Linkedin'>
+                            <img src={Linkedin} alt="Linkedin icon" height={60} />
+                            <div class="linea-con-bolas"></div>
+                            <p>check out my profile</p>
                         </div>
                         <div className='GetinT__Github'>
-
+                            <img src={Github} alt="Github icon" height={60} />
+                            <div class="linea-con-bolasB"></div>
+                            <p></p>
                         </div>
                     </div>
                 </div>
